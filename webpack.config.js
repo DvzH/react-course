8cb40109d50b3198061fc83de2011a8e819e48e9
DvzH <Diviz@github.com>
2018-03-21 18:30:24 +0530

    changesForEnv

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const webpack=require('webpack');
+const webpack = require('webpack');
 
-process.env.NODE_ENV=process.env.NODE_ENV||'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if(process.env.NODE_ENV==='development'){
-  require('dotenv').config({path:'.env.development'});
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' });
 }
 //envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV.trim() + '.env'));
 
-console.log("NODE_ENV",process.env.NODE_ENV); 
-console.log("API_KEY",process.env.APP_KEY);
+console.log("NODE_ENV", process.env.NODE_ENV);
+console.log("API_KEY", process.env.APP_KEY);
 
 module.exports = (env) => {
   const isProduction = env === 'production';
@@ -18,7 +18,7 @@ module.exports = (env) => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public','dist'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
@@ -32,15 +32,15 @@ module.exports = (env) => {
         use: CSSExtract.extract({
           use: [
             {
-              loader:'css-loader',
-              options:{
-                sourceMap:true
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
               }
             },
             {
-              loader:'sass-loader',
-              options:{
-                sourceMap:true
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
               }
             }
           ]
@@ -50,19 +50,19 @@ module.exports = (env) => {
     plugins: [
       CSSExtract,
       new webpack.DefinePlugin({
-        'process.env.FIREBASE_API_KEY':JSON.stringify(process.env.FIREBASE_API_KEY),
-        'process.env.FIREBASE_AUTH_DOMAIN':JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_DATABASE_URL':JSON.stringify(process.env.DATABASE_URL),
-        'process.env.FIREBASE_PROJECT_ID':JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_STORAGE_BUCKET':JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID':JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
       })
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
-      publicPath:'/dist/'
+      publicPath: '/dist/'
     }
   };
 }
