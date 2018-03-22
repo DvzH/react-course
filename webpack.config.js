6efbaae23d21a8565d2958ebb3f737e50d6bbb4a
DvzH <Diviz@github.com>
2018-03-22 12:38:36 +0530

    DatabaseConfiguration

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ if (process.env.NODE_ENV === 'development') {
 
 console.log("NODE_ENV", process.env.NODE_ENV);
 console.log("API_KEY", process.env.FIREBASE_API_KEY);
+console.log("database__21",process.env.FIREBASE_DATABASE_URL);
 
 module.exports = (env) => {
   const isProduction = env === 'production';
@@ -52,7 +53,7 @@ module.exports = (env) => {
       new webpack.DefinePlugin({
         'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
         'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
         'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
         'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
         'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
